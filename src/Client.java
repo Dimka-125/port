5de7fd37125fbb8eca82eda85acf4fac2f99bcfd
Dmitrii Vasilev <1dmirty1@gmail.com>
2025-05-14 12:23:06 +0300

    Initial Comment

---
+++ src/Client.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+
+    public static void main (String[] args) {
+        try (Socket clientSocket = new Socket("localhost", 8080);
+
+        PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+        {System.out.println("Connected");
+        writer.println("Dimka");
+        System.out.println("Прислано: " + reader.readLine());
+    } catch (IOException e) {
+    throw new RuntimeException(e);
+    }
+    }
+    }
